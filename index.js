03ca6db942a2ca2a2d0d2c92c5f29447f161440a
Aliaksandr Vechar <Aliaksandr.Vechar@itechart-group.com>
2021-08-19 15:46:53 +0300

    initial commitf

---
+++ index.js
@@ -0,0 +1,14 @@
+
+const express = require('express')
+
+const app = express()
+
+app.get('/', (req, res) => {
+  res.send({hi: 'there'})
+    }
+)
+
+const PORT = process.env.PORT
+app.listen(5000, function (){
+  console.log('listen on port 5000')
+})
