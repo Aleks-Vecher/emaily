64f89958f8aa3768996b57d6277cdb157b2e0554
Aliaksandr Vechar <Aliaksandr.Vechar@itechart-group.com>
2021-08-19 17:22:52 +0300

    third commit

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ app.get('/', (req, res) => {
     }
 )
 
-const PORT = process.env.PORT
+const PORT = process.env.PORT || 5000
 app.listen(5000, function (){
   console.log('listen on port 5000')
 })
