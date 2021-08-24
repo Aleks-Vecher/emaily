7f59a6a518996fe2e87fa28932eabde26b88f9e6
Aliaksandr Vechar <Aliaksandr.Vechar@itechart-group.com>
2021-08-24 11:13:18 +0300

    mongo

--- index.js
+++ index.js
@@ -1,14 +1,23 @@
-
 const express = require('express')
+const  mongoose = require('mongoose')
+const keys = require('./config/keys')
+require('./services/passport')
+
+
+mongoose.connect(keys.mongoURI, {
+  useNewUrlParser: true,
+  useCreateIndex: true,
+  useUnifiedTopology: true,
+});
 
 const app = express()
 
-app.get('/', (req, res) => {
-  res.send({hi: 'friend'})
-    }
-)
+require('./routes/authRoutes')(app)
+
+//console.developers.google.com
 
 const PORT = process.env.PORT || 5000
-app.listen( PORT, function (){
+app.listen(PORT, function () {
   console.log('listen on port 5000')
 })
+
