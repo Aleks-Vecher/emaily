4a02bb29daed1abab3fe58386a44e2c1531d234d
Aleks-Vecher <aleks.vecher@gmail.com>
2021-09-09 17:21:56 +0300

    final

--- config/prod.js
+++ config/prod.js
@@ -4,10 +4,10 @@
 module.exports = {
   googleClientID: process.env.GOOGLE_CLIENT_ID ,
   googleClientSecret: process.env.GOOGLE_CLIENT_SECRET,
-  mongoURI: process.env.MONGO_URI ,
-  cookieKey: process.env.COOKIE_KEY ,  // random key
-  stripePublishableKey: process.env.STRIPE_PUBLISHABLE_KEY ,
-  stripeSecretKey: process.env.STRIPE_SECRET_KEY ,
+  mongoURI: process.env.MONGO_URI,
+  cookieKey: process.env.COOKIE_KEY,  // random key
+  stripePublishableKey: process.env.STRIPE_PUBLISHABLE_KEY,
+  stripeSecretKey: process.env.STRIPE_SECRET_KEY,
   MailGunApiKey: process.env.MAILGUN_API_KEY,
   MailGunDomain: process.env.MAILGUN_DOMAIN_KEY,
   redirectDomain: process.env.REDIRECT_DOMAIN,
